3c4c0aba3a22c3760239b7fe782f0213db945ed8
Willy <hn12404988@hotmail.com>
2017-04-08 10:38:44 +0800

    Update

--- cppconn/install.sh
+++ cppconn/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+yum install -y boost-devel
 rpm -i mysql-connector-c++-1.1.7-linux-el7-x86-64bit.rpm
 rpm -i mysql-connector-c-devel-6.1.6-1.el7.x86_64.rpm
--- emacs/install.sh
+++ emacs/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yum -y install gtk+-devel gtk2-devel libXpm-devel libpng-devel giflib-devel libtiff-devel libjpeg-devel ncurses-devel gpm-devel dbus-devel dbus-glib-devel dbus-python GConf2-devel pkgconfig
 cd /tmp && \
-	wget http://ftp.kaist.ac.kr/gnu/gnu/emacs/emacs-24.5.tar.xz && \
-	tar xzf emacs-24.5.tar.xz && \
+	wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz && \
+	tar xzf emacs-24.5.tar.gz && \
 	cd emacs-24.5 && \
 	./configure --prefix=/usr/local --with-x-toolkit=gtk --without-gsettings && \
 	make && make install && make clean && \
-	rm -rf /tmp/emacs-24.5.tar.xz && rm -rf /tmp/emacs-24.5
+	rm -rf /tmp/emacs-24.5.tar.gz && rm -rf /tmp/emacs-24.5
