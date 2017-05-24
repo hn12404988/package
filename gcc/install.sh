2b8d143cec67696831c385e63bdfbe2ba9ca7a90
Willy <hn12404988@hotmail.com>
2017-05-24 09:47:06 +0800

    gcc

--- gcc/install.sh
+++ gcc/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Install GCC 5.4
-yum install -y libmpc-devel mpfr-devel gmp-devel
+yum install -y tar libmpc-devel mpfr-devel gmp-devel
 yum install -y zlib-devel*
 mkdir /tmp/gcc && cd /tmp/gcc
 cd /tmp/gcc
 curl ftp://ftp.gnu.org/pub/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.bz2 -O
-tar xvfj gcc-5.4.0.tar.bz2
-cd gcc-5.4.0
+tar xvfj tmp/gcc/gcc-5.4.0.tar.bz2
+cd tmp/gcc/gcc-5.4.0
 mkdir /usr/local/gcc_5.4
 ./configure --prefix=/usr/local/gcc_5.4 --with-system-zlib --disable-multilib --enable-languages=c,c++
 make -j 4
